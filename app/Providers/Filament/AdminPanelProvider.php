5da0b8148864276d6c281d16546d67a5496fe8de
ronisaputra764 <ronisaputra764@gmail.com>
2025-03-07 02:52:17 +0700

    laravelcloud

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,7 +52,7 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
             ])
             ->authMiddleware([
-                Authenticate::class,
+                // Authenticate::class,
             ]);
     }
 }
